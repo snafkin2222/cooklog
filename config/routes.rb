4e5331ceb647ec00b168265f7600efc0477dacc1
snafkin2222 <buntarou719@gmail.com>
2021-07-19 04:05:46 +0900

    login実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get 'users/show'
   get 'users/index'
+  get    :login,     to: 'sessions#new'
+  post   :login,     to: 'sessions#create'
+  delete :logout,    to: 'sessions#destroy'
   root 'static_pages#home'
   get :signup,       to: 'users#new'
   resources :users
