c7b4af5c10591886808aa4fa94911d5de93f50ca
snafkin2222 <buntarou719@gmail.com>
2021-07-19 04:27:33 +0900

    login情報の維持

--- spec/requests/users_login_spec.rb
+++ spec/requests/users_login_spec.rb
@@ -20,12 +20,14 @@ RSpec.describe "ログイン", type: :request do
     delete logout_path
     expect(is_logged_in?).not_to be_truthy
     redirect_to root_url
+    delete logout_path
+    follow_redirect!
   end
 
   it "無効なユーザーでログイン" do
     get login_path
     post login_path, params: { session: { email: "xxx@example.com",
-                                         password: user.password } }
+                                          password: user.password } }
     expect(is_logged_in?).not_to be_truthy
   end
 end
