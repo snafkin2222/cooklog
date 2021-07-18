64982ae4d8e5e69c4a3b34c34d1581d9126c4882
snafkin2222 <buntarou719@gmail.com>
2021-07-19 02:14:19 +0900

    rubocop

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-
   describe "GET /new" do
     it "returns http success" do
       get "/users/new"
@@ -22,5 +21,4 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
-
 end
