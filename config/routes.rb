a02044cce4b6a15120268799a37802ae6c1b0d6f
snafkin2222 <buntarou719@gmail.com>
2021-08-03 22:43:39 +0900

    follow機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,13 @@ Rails.application.routes.draw do
   delete :logout,    to: 'sessions#destroy'
   root 'static_pages#home'
   get :signup,       to: 'users#new'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :dishes
   get :about,        to: 'static_pages#about'
   get :use_of_terms, to: 'static_pages#terms'
+  resources :relationships, only: [:create, :destroy]
 end
